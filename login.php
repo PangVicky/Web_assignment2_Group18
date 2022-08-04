09b971790e7fe01e1bc14db7a4a2f778bf73d1f7
Michael Riesberry <MikeRiesberry@hotmail.ca>
2022-08-04 12:37:42 -0400

    Updated login.php page to match .html page

--- login.php
+++ login.php
@@ -10,39 +10,61 @@
 </head>
 <body id="loginbody">
 <?php include 'header.php' ?>
-<div id="error"></div>
-<div class="formcontainer">
-        <h1 id="loginheader">User Login</h1>
+<body id="loginbody">
+    <div id="error"></div>
+    <div class="formcontainer">
         <hr>
-        <form name="form" action="signup.php" onsubmit="return validate();" method="post">
+        <form action="signup.php" method="post" id="form" onsubmit="return validate();">
+            <table>
+                <tr>
+                    <td>
+                        <div class="textfield">
+                            <label for="email">Email Address</label><br>
+                            <input type="text" name="email" id="email" placeholder="Email">
+                            <span class="warning" id="emailError"></span>
+                        </div>
+                    </td>
+                    <td><label for="username">User Name</label><br>
+                        <input type="text" name="username" id="username" placeholder="User name">
+                    </td>
+                </tr>
 
-            <div class="textfield">
-                <label for="email">Email Address</label><br>
-                <input type="text" name="email" id="email" placeholder="Email">
-                <span class="warning" id="emailError"></span>
-            </div>
+                <tr>
+                    <td>
+                        <div class="textfield">
+                            <label for="login">User Name</label><br>
+                            <input type="text" name="login" id="login" placeholder="User name">
+                            <span class="warning" id="loginError"></span>
+                        </div>
+                    </td>
+                    <td><label for="password2">Password</label><br>
+                        <input type="text" name="password2" id="password2" placeholder="Password">
+                    </td>
+                </tr>
 
-            <div class="textfield">
-                <label for="login">User Name</label><br>
-                <input type="text" name="login" id="login" placeholder="User name">
-                <span class="warning" id="loginError"></span>
-            </div>
+                <tr>
+                    <td>
+                        <div class="textfield">
+                            <label for="pass">Password</label><br>
+                            <input type="password" name="pass" id="pass" placeholder="Password">
+                            <span class="warning" id="passError"></span>
+                        </div>
+                    </td>
+                    <td><button id="loginbutton">Log In</button></td>
+                </tr>
 
-            <div class="textfield">
-                <label for="pass">Password</label><br>
-                <input type="password" name="pass" id="pass" placeholder="Password">
-                <span class="warning" id="passError"></span>
-            </div>
-        
-            <div class="textfield">
-                <label for="pass2">Re-type Password</label><br>
-                <input type="password" id="pass2" placeholder="Password">
-                <span class="warning" id="pass2Error"></span>
-            </div>
-
-            <button id="submit" type="submit">Sign-Up</button>
-            <button id="reset" type="reset">Reset</button>
-
+                <tr>
+                    <td>
+                        <div class="textfield">
+                            <label for="pass2">Re-type Password</label><br>
+                            <input type="password" id="pass2" placeholder="Password">
+                            <span class="warning" id="pass2Error"></span>
+                        </div>
+                    </td>
+                </tr>
+            </table>
+            <button type="submit" id="submit">Sign-Up</button>
+            <button type="reset" id="reset">Reset</button>
         </form>
     </div>
 
