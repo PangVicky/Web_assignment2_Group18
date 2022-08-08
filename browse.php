8401d2993dad7798e8dade6627d82137565a216b
Chunxia Pang <pang0064@algonquinlive.com>
2022-08-08 16:16:17 -0400

    search

--- browse.php
+++ browse.php
@@ -9,7 +9,7 @@
   <title>Home Page_WEBSITE NAME REQUIRED(NOT NETFLIX)</title>
   <link href="assignment2.css" rel="stylesheet" type="text/css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-  <script type="text/javascript" src="js/jquery/jquery-1.12.1.min.js"></script>
+  <!-- <script type="text/javascript" src="js/jquery/jquery-1.12.1.min.js"></script> -->
   <script src="search.js" defer></script>
 </head>
 
@@ -32,7 +32,7 @@
 
 <form class="mysearch" style="margin:auto;max-width:300px">
     <input id="searchbar" type="text" placeholder="Please input.." name="search2">
-    <button type="button" onclick="searchfunc()"><i class="fa fa-search"></i></button>
+    <button type="button" onclick="getOption(this)"><i class="fa fa-search"></i></button>
   
   <!-- create the filter area -->
   <select name="options" id="filter" onchange="getOption(this)">
